89a8ff921c99481f5169222fbfbf704ce28d647d
kirtisoglu <alaittinkirtisoglu@gmail.com>
2023-08-02 19:33:25 +0300

    Update Plan.py

--- Plan.py
+++ Plan.py
@@ -6,24 +6,28 @@
 # Define task lists of length 15, 20, 25, 30. 
 # Define their weight lists using parameters. Weights should be w_1, w_2, w_3, w_4.
 # And so we can change w_i's and test the result for different w_i's.
-# Define easy tasks for off days.
+# w_1's are easy tasks for off days. We will add a constraint for that.
 
 
 # Define the possible programs in the village. 
 # Create two different types of each program. 
 # One has an off day in the week, the other does not have. 
+# OR Ask program name, number of groups, and off day of the program as an input and skip the following.
+
 
 
 # Define off days of programs to assign easy tasks. 
 
 
-# Ask the number of each program and save them. 
-# Merge them in an order and calculate n.
+ 
+# Merge the programs in an order and calculate n.
+
 
 
 # Pick the first list whose length exceed n. 
 # Delete unneccesary tasks from the list. 
-# Save it as the main task list.
+# Define it as the main task list.
+
 
 
 # Now we know task lists, programs and their numbers, off days of programs, easy tasks. 
@@ -36,11 +40,14 @@
 
 
 
-# Define Solution Method and solve the problem.
+
+# Construct Simulated Annealing and solve the problem.
+
 
 
 
 # Convert your solution vector to a daily-task assignment of groups. 
+
 
 
 
