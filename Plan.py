fce9146b2b059b5c3c56941b564aca7dec85e984
kirtisoglu <alaittinkirtisoglu@gmail.com>
2023-08-02 18:54:51 +0300

    Plan

---
+++ Plan.py
@@ -0,0 +1,48 @@
+# We will assign daily-tasks to groups every week. 
+# This program does not remember what are assigned the last week. 
+# Define the libraries you need.
+
+
+# Define task lists of length 15, 20, 25, 30. 
+# Define their weight lists using parameters. Weights should be w_1, w_2, w_3, w_4.
+# And so we can change w_i's and test the result for different w_i's.
+# Define easy tasks for off days.
+
+
+# Define the possible programs in the village. 
+# Create two different type of high school programs. 
+# One has an off day in the week, the other does not have. 
+
+
+# Define off days of programs to assign easy tasks. 
+
+
+# Ask the number of each program and save them. 
+# Merge them in an order and calculate n.
+
+
+# Pick the first list whose length exceed n. 
+# Delete unneccesary tasks from the list. 
+# Save it as the main task list.
+
+
+# Now we know task lists, programs and their numbers, off days of programs, easy tasks. 
+# Do we need to know something else? 
+# I think we can now define the objective function by using n, weights, and the main task list. 
+
+
+
+# Define constraints.
+
+
+
+# Define Solution Method and solve the problem.
+
+
+
+# Convert your solution vector to a daily-task assignment of groups. 
+
+
+
+# Print the task assignment as a table. Save the table.
+
