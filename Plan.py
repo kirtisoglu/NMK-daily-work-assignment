588ca76988351a260a843de5e27e0a9bafd67ef3
ccbolat <bolatcemre@gmail.com>
2023-08-06 18:13:44 +0300

    Gorev dagilimi task ve input okuma eklendi

--- Plan.py
+++ Plan.py
@@ -1,6 +1,7 @@
 # We will assign daily-tasks to groups every week. 
 # This program does not remember what are assigned the last week. 
 # Define the libraries you need.
+import pandas as pd
 
 
 # Define task lists of length 15, 20, 25, 30. 
@@ -8,34 +9,64 @@
 # And so we can change w_i's and test the result for different w_i's.
 # w_1's are easy tasks for off days. We will add a constraint for that.
 
+print("Hoş geldiniz")
+
+excel_file = './ornekgorev.xlsx' 
+sheet_name = 'Sayfa1'
+print("Görevleriniz {} dosyasında,{} sayfasından okunuyor".format(excel_file, sheet_name))
 
 
+data = pd.read_excel(excel_file, sheet_name=sheet_name)
+allTasks = data.values
 
 
-# Define the possible programs in the village. 
 # Create two different types of each program. 
 # One has an off day in the week, the other does not have. 
 # OR Ask program name, number of groups, and off day of the program as an input and skip the following.
 
+while True:
+    try:
+        num_of_groups_with_off_day = int(input("Çarşamba günü tatil yapacak olan grupların sayısını giriniz: "))
+        if num_of_groups_with_off_day < 0:
+            print("Lütfen 0 veya daha büyük bir değer girin.")
+        else:
+            break
+    except ValueError:
+        print("Geçerli bir tam sayı girmediniz. Lütfen tekrar deneyin.")
 
+while True:
+    try:
+        num_of_groups_without_off_day = int(input("Tatil yapmayan grupların sayısını giriniz: "))
+        if num_of_groups_without_off_day < 0:
+            print("Lütfen 0 veya daha büyük bir değer girin.")
+        else:
+            break
+    except ValueError:
+        print("Geçerli bir tam sayı girmediniz. Lütfen tekrar deneyin.")
 
-# Define off days of programs to assign easy tasks. 
-
-
- 
 # Merge the programs in an order and calculate n.
 
+allGroups = []
 
+for i in range(0, num_of_groups_with_off_day +num_of_groups_without_off_day):
+    allGroups.append({
+        "id": i + 1,
+        "hasOffDay": i< num_of_groups_with_off_day,
+    })
+    
+allGroupsLength = len(allGroups)
 
-# Pick the first list whose length exceed n. 
-# Delete unneccesary tasks from the list. 
-# Define it as the main task list.
+taskList = []
 
+if allGroupsLength > 25 :
+    taskList = allTasks[0:allGroupsLength]
+elif  allGroupsLength > 20 : 
+    taskList = allTasks[0:allGroupsLength]
+elif  allGroupsLength > 15 : 
+    taskList = allTasks[0:allGroupsLength]
+else: 
+    taskList = allTasks[0:allGroupsLength]
 
-
-# Now we know task lists, programs and their numbers, off days of programs, easy tasks. 
-# Do we need to know something else? 
-# I think we can now define the objective function by using n, weights, and the main task list. 
 
 
 
