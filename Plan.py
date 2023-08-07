4af0d5cf6b204f101cd322ff7bf9002ab24ef6b1
kirtisoglu <alaittinkirtisoglu@gmail.com>
2023-08-07 13:12:32 +0300

    Update Plan.py

--- Plan.py
+++ Plan.py
@@ -13,11 +13,22 @@ print("Hoş geldiniz")
 
 excel_file = './ornekgorev.xlsx' 
 sheet_name = 'Sayfa1'
-print("Görevleriniz {} dosyasında,{} sayfasından okunuyor".format(excel_file, sheet_name))
 
+print("Görevleriniz {} dosyasında,{} sayfasından okunuyor".format(excel_file, sheet_name))
+## Why do you need this?
 
 data = pd.read_excel(excel_file, sheet_name=sheet_name)
 allTasks = data.values
+
+## Define the first column as task names and the corresponding second column as the weight of it.
+
+
+
+
+## Define w_1, w_2, w_3, w_4.
+## For example, let's take w_1=1, w_2=3, w_3=5, w=4=7 and see what happens.
+
+
 
 
 # Create two different types of each program. 
@@ -44,7 +55,26 @@ while True:
     except ValueError:
         print("Geçerli bir tam sayı girmediniz. Lütfen tekrar deneyin.")
 
+## We might have different off-days, not only wednesday.
+## Call it easy-task-day. For example, undergrad programs might have
+## both wednesday and sunday to have easy tasks since sunday is the last day
+## for their program and some of the group members leave the village.  
+## So, it is possible to have 2 easy-task-day for a group. 
+ 
+## Imagine you learned the easy-task-day of a group. How do you teach this to
+## your integer program? For the decision variable x_{ijk}, k indicates the days of week. 
+## k=3 for wednesday. Define a set of easy tasks E. Here is the constraint:
+## x_{ij3}=0 for groups i (i must be defined by using inputs) for all j in E_c, the complement of E.
+## Since we have a constraint to assign a task every day to each group, 
+## a task is assigned to group i on Wednesday from E.
+
+
+
 # Merge the programs in an order and calculate n.
+# Pick the first list whose length exceed n. 
+# Delete unneccesary tasks from the list. 
+# Define it as the main task list.
+
 
 allGroups = []
 
@@ -70,7 +100,18 @@ else:
 
 
 
+
+
+
+# Now we know task lists, programs and their numbers, off days of programs, easy tasks. 
+# Dfine the objective function by using n, weights, and the main task list. 
+
+
+
+
+
 # Define constraints.
+
 
 
 
